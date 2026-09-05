d47b30f162ae0733fb6ff7198b2bcee90b5f2ba1
Sisay Chala <sisayie@gmail.com>
2026-09-05 19:11:34 +0200

    Readme updated

--- aws-ml-stack.sh
+++ aws-ml-stack.sh
@@ -13,8 +13,8 @@ STACK_NAME="${STACK_NAME:-ml-stack}"
 aws configure 
 
 # GitHub raw URL to the CloudFormation template.
-TEMPLATE_URL="${TEMPLATE_URL:-https://raw.githubusercontent.com/ORG/REPO/main/cloudformation/template.yaml}"
-
+#TEMPLATE_URL="${TEMPLATE_URL:-https://raw.githubusercontent.com/ORG/REPO/main/cloudformation/template.yaml}"
+TEMPLATE_URL="${https://raw.githubusercontent.com/sisayie/aws-course/refs/heads/main/cloudformation/ml-stack-lambda.yml}"
 # Optional CloudFormation parameters.
 PARAMETERS=(
   "ParameterKey=Environment,ParameterValue=dev"
