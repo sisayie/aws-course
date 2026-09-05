954361c5af2e0e4f8120c0fd315292182d9892d4
Sisay Chala <sisayie@gmail.com>
2026-09-05 19:44:01 +0200

    Readme updated

--- aws-ml-stack.sh
+++ aws-ml-stack.sh
@@ -14,7 +14,8 @@ aws configure
 
 # GitHub raw URL to the CloudFormation template.
 #TEMPLATE_URL="${TEMPLATE_URL:-https://raw.githubusercontent.com/ORG/REPO/main/cloudformation/template.yaml}"
-TEMPLATE_URL="${TEMPLATE_URL:-https://raw.githubusercontent.com/sisayie/aws-course/refs/heads/main/cloudformation/exam-score-ml-stack_v3.yml}"
+#TEMPLATE_URL="${TEMPLATE_URL:-https://raw.githubusercontent.com/sisayie/aws-course/refs/heads/main/cloudformation/exam-score-ml-stack_v3.yml}"
+TEMPLATE_URL="${TEMPLATE_URL:-https://raw.githubusercontent.com/sisayie/aws-course/refs/heads/main/cloudformation/sagemaker-s3-processing.yml}"
 
 # Optional CloudFormation parameters.
 PARAMETERS=(
